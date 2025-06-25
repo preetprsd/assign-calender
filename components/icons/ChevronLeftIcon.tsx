4df1baf132b54d041cc2e00a31aae5c0d25d7dbe
globals <kaaliputt@proton.me>
2025-06-25 13:14:32 +0530

    Added vite-plugin-svgr for SVG support for lefticon,righticon,trashicon,xmarkicon

---
+++ components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as ChevronLeft } from './chevron-left.svg';
+
+interface ChevronLeftIconProps extends React.SVGProps<SVGSVGElement> {}
+
+const ChevronLeftIcon: React.FC<ChevronLeftIconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+  </svg>
+);
+
+export default ChevronLeftIcon;
---
+++ components/icons/XMarkIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface XMarkIconProps extends React.SVGProps<SVGSVGElement> {}
+
+const XMarkIcon: React.FC<XMarkIconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg>
+);
+
+export default XMarkIcon;
